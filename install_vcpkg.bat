ff6ef988fd1be390e723b62dc0731d31bfa88f6e
손창대 <changdae20@naver.com>
2022-06-09 09:37:36 +0900

    change protobuf version 3.9.1 to latest

--- install_vcpkg.bat
+++ install_vcpkg.bat
@@ -1,6 +1,5 @@
 git clone https://github.com/microsoft/vcpkg
 cd vcpkg
-git checkout 4e9d79e36dac3b2115f14d663cc47dff3c8e58d5
 call bootstrap-vcpkg.bat -disableMetrics
 .\vcpkg install protobuf protobuf:x64-windows
 cd ../
