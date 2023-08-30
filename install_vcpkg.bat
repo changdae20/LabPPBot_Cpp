f5b693b58606a8aa9ff2c1b26fe48285047d48e3
손창대 <changdae20@naver.com>
2023-08-30 10:38:16 +0900

    vcpkg revision 지정

--- install_vcpkg.bat
+++ install_vcpkg.bat
@@ -1,5 +1,6 @@
 git clone https://github.com/microsoft/vcpkg
 cd vcpkg
+git reset --hard f6a5d4e8eb7476b8d7fc12a56dff300c1c986131
 call bootstrap-vcpkg.bat -disableMetrics
 .\vcpkg install protobuf protobuf:x64-windows fmt fmt:x64-windows
 cd ../
